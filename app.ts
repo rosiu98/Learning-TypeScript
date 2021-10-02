3f2444a0b41fa07ae95cd5390b63c4bece9a1dd6
Paweł <rosiu978@gmail.com>
2021-10-02 17:06:05 +0200

    basic

---
+++ app.ts
@@ -0,0 +1,16 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+
+function printResult(num: number): void {
+  console.log("Result" + num);
+}
+
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+combineValues = printResult;
+
+console.log(combineValues(8, 8));
